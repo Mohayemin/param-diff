2f26669377b6fa45ee39fa25d178109c64454105
mohayemin <mohayeminul.islam@gmail.com>
2019-10-27 22:48:11 +0600

    pass: findAll_nodiff_emptyList

--- src/main/java/paramdiff/ParamDiffFinder.java
+++ src/main/java/paramdiff/ParamDiffFinder.java
@@ -1,9 +1,10 @@
 package paramdiff;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ParamDiffFinder {
     public List<ParamDiff> findParamAddition(String oldCode, String newCode) {
-        return null;
+        return new ArrayList<>();
     }
 }
