250c23be2eb0be68257e1aa608d132baa53833dd
mohayemin <mohayeminul.islam@gmail.com>
2019-10-27 22:50:44 +0600

    refactor: test rename test method to match code method

--- src/test/java/paramdiff/ParamDiffFinderTest.java
+++ src/test/java/paramdiff/ParamDiffFinderTest.java
@@ -10,7 +10,7 @@ import java.nio.file.Paths;
 
 public class ParamDiffFinderTest {
     @Test
-    public void findAll_nodiff_emptyList() throws IOException {
+    public void findParamAddition_nodiff_emptyList() throws IOException {
         var filePath = new File("src/test/resources/test_data/Calculator.java").getAbsolutePath();
         var code = Files.readString(Paths.get(filePath));
         var paramDiff = new ParamDiffFinder().findParamAddition(code, code);
