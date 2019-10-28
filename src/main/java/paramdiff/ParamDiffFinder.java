d2a860244dcbebe0300b0babb76f1eeb1ffb0e6e
mohayemin <mohayeminul.islam@gmail.com>
2019-10-28 09:26:48 +0600

    refactor: remove unnecessary nesting

--- src/main/java/paramdiff/ParamDiffFinder.java
+++ src/main/java/paramdiff/ParamDiffFinder.java
@@ -9,14 +9,13 @@ public class ParamDiffFinder {
     public List<ParamAdditionDiff> findParamAddition(String oldCodeText, String newCodeText) {
         if (oldCodeText == newCodeText) {
             return new ArrayList<>();
-        } else {
-            var newCode = StaticJavaParser.parse(newCodeText);
-
-            var newMethod = newCode.getType(0).getMethods().get(0);
-            var addedParamIndex = newMethod.getParameters().size()-1;
-            var addedParam = newMethod.getParameter(2).getTypeAsString();
-            var diff = new ParamAdditionDiff("", "", addedParamIndex, addedParam);
-            return List.of(diff);
         }
+
+        var newCode = StaticJavaParser.parse(newCodeText);
+        var newMethod = newCode.getType(0).getMethods().get(0);
+        var addedParamIndex = newMethod.getParameters().size() - 1;
+        var addedParam = newMethod.getParameter(2).getTypeAsString();
+        var diff = new ParamAdditionDiff("", "", addedParamIndex, addedParam);
+        return List.of(diff);
     }
 }
