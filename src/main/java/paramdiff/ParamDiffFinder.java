cd43f9218415cb6162f69a8dec0d6e9b13af910f
mohayemin <mohayeminul.islam@gmail.com>
2019-10-31 00:29:10 +0600

    misc refactor

--- src/main/java/paramdiff/ParamDiffFinder.java
+++ src/main/java/paramdiff/ParamDiffFinder.java
@@ -10,8 +10,10 @@ import com.github.javaparser.ast.type.Type;
 import util.Sequences;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class ParamDiffFinder {
     public List<ParamAdditionDiff> findParamAddition(String oldCodeText, String newCodeText) {
@@ -37,42 +39,45 @@ public class ParamDiffFinder {
         return diffs;
     }
 
-    private List<ParamAdditionDiff> findParamAdditions(TypeDeclaration<?> oldType, TypeDeclaration newType) {
+    private List<ParamAdditionDiff> findParamAdditions(TypeDeclaration<?> oldType, TypeDeclaration<?> newType) {
         if (newType == null)
             return List.of();
 
-        var paramModifiedMethods = new ArrayList<MethodDeclaration>();
-        List<MethodDeclaration> newMethods = new ArrayList<>(newType.getMethods());
-        for (var oldMethod : oldType.getMethods()) {
-            var newMethod = findMatchingMethod(newType, oldMethod);
-            if (newMethod != null) {
-                newMethods.remove(newMethod);
-            } else {
-                paramModifiedMethods.add(oldMethod);
-            }
-        }
+        var oldCandidateMethods = new HashSet<>(oldType.getMethods());
+        var newCandidateMethods = new HashSet<>(newType.getMethods());
+        removeUnchangedMethods(oldType, newType, oldCandidateMethods, newCandidateMethods);
 
         var diffs = new ArrayList<ParamAdditionDiff>();
-        for (var oldMethod : paramModifiedMethods) {
-            var addedParams = newMethods.stream()
-                    .filter(m -> m.getNameAsString().equals(oldMethod.getNameAsString()))
-                    .filter(m -> isParamSuperSet(m, oldMethod))
-                    .collect(Collectors.toList());
-            if (addedParams.size() == 0)
-                continue;
-
-            diffs.add(new ParamAdditionDiff(oldMethod, addedParams.get(0)));
+        for (var oldMethod : oldCandidateMethods) {
+            findParamAddedMethods(newCandidateMethods, oldMethod)
+                    .forEach(m -> diffs.add(new ParamAdditionDiff(oldMethod, m)));
         }
 
         return diffs;
     }
 
-    private boolean isParamSuperSet(MethodDeclaration sup, MethodDeclaration sub) {
-        var supParams = getParamTypes(sup);
-        var subParams = getParamTypes(sub);
-        if (supParams.size() == subParams.size())
-            return false;
+    private Stream<MethodDeclaration> findParamAddedMethods(HashSet<MethodDeclaration> newCandidateMethods,
+                                                            MethodDeclaration oldMethod) {
+        var overloads = newCandidateMethods.stream()
+                .filter(m -> m.getNameAsString().equals(oldMethod.getNameAsString()));
+        var overloadsWithMoreParams =
+                overloads.filter(m -> m.getParameters().size() > oldMethod.getParameters().size());
+        return overloadsWithMoreParams
+                .filter(m -> isParamSuperSet(m, oldMethod));
+    }
 
+    private void removeUnchangedMethods(TypeDeclaration<?> oldType, TypeDeclaration newType,
+                                        HashSet<MethodDeclaration> oldCandidates, HashSet newCandidates) {
+        for (var oldMethod : oldType.getMethods()) {
+            var newMethod = findMatchingMethod(newType, oldMethod);
+            if (newMethod != null) {
+                newCandidates.remove(newMethod);
+                oldCandidates.remove(oldMethod);
+            }
+        }
+    }
+
+    private boolean isParamSuperSet(MethodDeclaration sup, MethodDeclaration sub) {
         return Sequences.containsNonContinuous(getParamTypes(sup), getParamTypes(sub));
     }
 
@@ -84,10 +89,10 @@ public class ParamDiffFinder {
         var params = method.getParameters().stream()
                 .map(parameter -> parameter.getNameAsString())
                 .collect(Collectors.toList()).toArray(new String[0]);
-        List<MethodDeclaration> mathichMethodList = type.getMethodsBySignature(method.getNameAsString(), params);
+        List<MethodDeclaration> matchingMethodList = type.getMethodsBySignature(method.getNameAsString(), params);
 
-        if (mathichMethodList.size() > 0) {
-            return mathichMethodList.get(0);
+        if (matchingMethodList.size() > 0) {
+            return matchingMethodList.get(0);
         }
         return null;
     }
